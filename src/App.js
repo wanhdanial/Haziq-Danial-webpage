d8d6258d6e6d19d3cdf8dc24917a37b07ab66da0
Haziq Danial <haziq5746@gmail.com>
2026-05-26 18:46:14 +0800

    Update background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
+import profilePhoto from './assets/starwars.jpg';
+import bgImage from './assets/background.png'
+import  './App.css';
 
 function App() {
   return (
-    <div style={{ fontFamily: 'sans-serif', maxWidth: 900, margin: '0 auto', padding: 80 }}>
-      <h1>Hi, I'm Haziq Danial 👋</h1>
+    
+    <div style={{ 
+      backgroundImage: `url(${bgImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      minHeight: '100vh',
+      fontFamily: 'sans-serif', 
+      maxWidth: 1000, 
+      margin: '0 auto', 
+      padding: 80 }}>
+    
+      <img
+        src={profilePhoto}
+        alt='Profile Photo'
+        style={{
+          width: 150,
+          height: 150,
+          borderRadius: '50%',
+          objectFit: 'cover',
+          marginBottom: 20,
+        }}
+        />
+    
+      <h1>Hi, I'm Haziq Danial 👋</h1> 
       <p>I'm a developer who loves building things for the web.</p>
 
       <h2>Project</h2>
